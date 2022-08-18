8205fed84841e76217122b8a69993106ed650b99
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-08-18 09:58:09 +0900

    add version

--- cmd/ecsta/main.go
+++ cmd/ecsta/main.go
@@ -7,8 +7,11 @@ import (
 	"github.com/fujiwara/ecsta"
 )
 
+var version = "current"
+
 func main() {
 	cliApp := ecsta.NewCLI()
+	cliApp.Version = version
 	if err := cliApp.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
