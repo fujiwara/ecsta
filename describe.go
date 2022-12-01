801d64600067f9b9d925cc6e2de202a2e18da0ba
fujiwara <fujiwara.shunichiro@gmail.com>
2022-12-01 05:18:31 +0000

    add --family and --service to describe

--- describe.go
+++ describe.go
@@ -6,14 +6,16 @@ import (
 )
 
 type DescribeOption struct {
-	ID string `help:"task ID"`
+	ID      string  `help:"task ID"`
+	Family  *string `help:"task definition family name"`
+	Service *string `help:"ECS service name"`
 }
 
 func (app *Ecsta) RunDescribe(ctx context.Context, opt *DescribeOption) error {
 	if err := app.SetCluster(ctx); err != nil {
 		return err
 	}
-	task, err := app.findTask(ctx, &optionFindTask{id: opt.ID})
+	task, err := app.findTask(ctx, &optionFindTask{id: opt.ID, family: opt.Family, service: opt.Service})
 	if err != nil {
 		return fmt.Errorf("failed to select tasks: %w", err)
 	}
