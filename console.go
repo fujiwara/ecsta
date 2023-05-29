68cf95e5c82bb611195bbb871895a8419eca056f
fujiwara <fujiwara.shunichiro@gmail.com>
2023-05-29 17:45:13 +0900

    complete task ID

--- console.go
+++ console.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -23,40 +22,57 @@ type Console struct {
 	List        *ListOption        `cmd:"" help:"List tasks" aliases:"ls"`
 	Logs        *LogsOption        `cmd:"" help:"Show log messages of a task"`
 	Portforward *PortforwardOption `cmd:"" help:"Forward a port of a task"`
-	Select      *SelectOption      `cmd:"" help:"Select a task"`
+	SelectTask  *SelectTaskOption  `cmd:"" name:"task" help:"Select a task"`
 	Stop        *StopOption        `cmd:"" help:"Stop a task"`
 	Trace       *TraceOption       `cmd:"" help:"Trace a task"`
 
-	Exit  struct{} `cmd:"" help:"Exit console" aliases:"quit"`
-	Help  struct{} `cmd:"" help:"Show help"`
-	Reset struct{} `cmd:"" help:"Reset console"`
+	Exit struct{} `cmd:"" help:"Exit console" aliases:"quit"`
+	Help struct{} `cmd:"" help:"Show help"`
 }
 
-var consoleCompleter = readline.NewPrefixCompleter(
-	readline.PcItem("describe"),
-	readline.PcItem("exec"),
-	readline.PcItem("help"),
-	readline.PcItem("--help"),
-	readline.PcItem("list"),
-	readline.PcItem("logs"),
-	readline.PcItem("portforward"),
-	readline.PcItem("select"),
-	readline.PcItem("stop"),
-	readline.PcItem("trace"),
-	readline.PcItem("exit"),
-	readline.PcItem("reset"),
-	readline.PcItem("quit"),
-)
+type ConsoleState struct {
+	TaskID   string
+	ReadLine *readline.Instance
+}
+
+func (app *Ecsta) newConsoleCompleter(ctx context.Context) readline.AutoCompleter {
+	return readline.NewPrefixCompleter(
+		readline.PcItem("describe"),
+		readline.PcItem("exec"),
+		readline.PcItem("help"),
+		readline.PcItem("--help"),
+		readline.PcItem("list"),
+		readline.PcItem("logs"),
+		readline.PcItem("portforward"),
+		readline.PcItem("stop"),
+		readline.PcItem("trace"),
+		readline.PcItem("exit"),
+		readline.PcItem("quit"),
+		readline.PcItem("task", readline.PcItemDynamic(func(line string) []string {
+			tasks, err := app.listTasks(ctx, &optionListTasks{})
+			if err != nil {
+				log.Println("[error]", err)
+			}
+			var names []string
+			for _, task := range tasks {
+				names = append(names, arnToName(*task.TaskArn))
+			}
+			return names
+		})),
+	)
+}
 
 func (app *Ecsta) RunConsole(ctx context.Context, opt *ConsoleOption) error {
 	if err := app.SetCluster(ctx); err != nil {
 		return err
 	}
+	s := &ConsoleState{}
 
-	l, err := readline.NewEx(&readline.Config{
+	var err error
+	s.ReadLine, err = readline.NewEx(&readline.Config{
 		Prompt:            fmt.Sprintf("%s> ", app.cluster),
 		HistoryFile:       filepath.Join(os.Getenv("HOME"), ".local/state/ecsta/history"),
-		AutoComplete:      consoleCompleter,
+		AutoComplete:      app.newConsoleCompleter(ctx),
 		InterruptPrompt:   "^C",
 		EOFPrompt:         "exit",
 		HistorySearchFold: true,
@@ -64,8 +80,8 @@ func (app *Ecsta) RunConsole(ctx context.Context, opt *ConsoleOption) error {
 	if err != nil {
 		return err
 	}
-	defer l.Close()
-	l.CaptureExitSignal()
+	defer s.ReadLine.Close()
+	s.ReadLine.CaptureExitSignal()
 
 	var console Console
 	var showHelp bool
@@ -75,17 +91,16 @@ func (app *Ecsta) RunConsole(ctx context.Context, opt *ConsoleOption) error {
 		return err
 	}
 
-	var taskID string
 INPUT:
 	for {
-		if taskID == "" {
-			l.SetPrompt(fmt.Sprintf("%s> ", app.cluster))
+		if s.TaskID == "" {
+			s.ReadLine.SetPrompt(fmt.Sprintf("%s> ", app.cluster))
 		} else {
-			l.SetPrompt(fmt.Sprintf("%s@%s> ", taskID, app.cluster))
+			s.ReadLine.SetPrompt(fmt.Sprintf("%s@%s> ", s.TaskID, app.cluster))
 		}
 		showHelp = false
 
-		line, err := l.Readline()
+		line, err := s.ReadLine.Readline()
 		if err == readline.ErrInterrupt {
 			if len(line) == 0 {
 				break
@@ -111,7 +126,7 @@ INPUT:
 			log.Println("[debug]", cmd, showHelp)
 			continue INPUT
 		}
-		if err := app.DispatchConsole(ctx, cmd, &console, &taskID); err != nil {
+		if err := app.DispatchConsole(ctx, cmd, &console, s); err != nil {
 			if err == io.EOF {
 				break
 			}
@@ -121,68 +136,66 @@ INPUT:
 	return nil
 }
 
-func (app *Ecsta) DispatchConsole(ctx context.Context, command string, console *Console, taskID *string) error {
+func (app *Ecsta) DispatchConsole(ctx context.Context, command string, console *Console, s *ConsoleState) error {
 	switch command {
 	case "exit", "quit":
 		return io.EOF
-	case "reset":
-		return exec.Command("reset").Run()
 	case "help":
 		return fmt.Errorf("use --help")
 	case "list":
 		return app.RunList(ctx, console.List)
-	case "select":
-		return app.RunSelect(ctx, console.Select, taskID)
+	case "task":
+		return app.RunSelectTask(ctx, console.SelectTask, s)
 	}
 
-	if *taskID == "" {
-		return fmt.Errorf("task ID is not specified. use `select` command")
+	if s.TaskID == "" {
+		return fmt.Errorf("no task is selected. use `task` command")
 	}
 
 	switch command {
 	case "describe":
-		console.Describe.ID = *taskID
+		console.Describe.ID = s.TaskID
 		return app.RunDescribe(ctx, console.Describe)
 	case "exec":
-		console.Exec.ID = *taskID
+		console.Exec.ID = s.TaskID
 		return app.RunExec(ctx, console.Exec)
 	case "logs":
-		console.Logs.ID = *taskID
+		console.Logs.ID = s.TaskID
 		return app.RunLogs(ctx, console.Logs)
 	case "portforward":
-		console.Portforward.ID = *taskID
+		console.Portforward.ID = s.TaskID
 		return app.RunPortforward(ctx, console.Portforward)
 	case "stop":
-		console.Stop.ID = *taskID
+		console.Stop.ID = s.TaskID
 		return app.RunStop(ctx, console.Stop)
 	case "trace":
-		console.Trace.ID = *taskID
+		console.Trace.ID = s.TaskID
 		return app.RunTrace(ctx, console.Trace)
 	}
 	return fmt.Errorf("unknown command: %s", command)
 }
 
-type SelectOption struct {
+type SelectTaskOption struct {
 	TaskID  string  `arg:"" optional:"" help:"task ID or prefix"`
 	Family  *string `help:"task definition family name"`
 	Service *string `help:"ECS service name"`
 }
 
-func (app *Ecsta) RunSelect(ctx context.Context, opt *SelectOption, selected *string) error {
+func (app *Ecsta) RunSelectTask(ctx context.Context, opt *SelectTaskOption, s *ConsoleState) error {
 	switch {
 	case opt.TaskID == "":
 		task, err := app.findTask(ctx, &optionFindTask{})
 		if err != nil {
 			return err
 		}
-		*selected = arnToName(*task.TaskArn)
+		s.TaskID = arnToName(*task.TaskArn)
 		return nil
 	case len(opt.TaskID) >= 32 && len(opt.TaskID) <= 36:
 		_ts, err := app.describeTasks(ctx, &optionDescribeTasks{ids: []string{opt.TaskID}})
 		if err != nil {
 			return err
 		} else if len(_ts) == 1 {
-			*selected = arnToName(*_ts[0].TaskArn)
+			s.TaskID = arnToName(*_ts[0].TaskArn)
 			return nil
 		} else {
 			return fmt.Errorf("taskID %s not found", opt.TaskID)
@@ -202,7 +215,7 @@ func (app *Ecsta) RunSelect(ctx context.Context, opt *SelectOption, selected *st
 		if len(foundTaskIDs) == 0 {
 			return fmt.Errorf("taskID %s not found", opt.TaskID)
 		} else if len(foundTaskIDs) == 1 {
-			*selected = foundTaskIDs[0]
+			s.TaskID = foundTaskIDs[0]
 			return nil
 		} else {
 			return fmt.Errorf("[error] taskID %s is ambiguous", opt.TaskID)
